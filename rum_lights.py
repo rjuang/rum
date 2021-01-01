4210ef27d9071c3e8d9d91413e8dfb175b38c22d
Ray Juang <rjuang@github.com>
2021-01-01 00:30:35 -0800

    Adding classes to abstract away the concept of lights.

---
+++ rum_lights.py
@@ -0,0 +1,117 @@
+""" Contains classes for controlling LED lights on the device. """
+
+
+class Light:
+    """ Interface for a light source on the device. """
+    def get(self): raise NotImplementedError()
+    def set(self, value, force_update=False): raise NotImplementedError()
+
+
+class ToggleLight(Light):
+    """ Interface for a light source that can be toggled on the device. """
+    def toggle(self): raise NotImplementedError
+
+
+class OnOffLight(ToggleLight):
+    """ Controls a light on the device that can be toggled on/off. """
+    def __init__(self, on_fn=None, off_fn=None, initial=False):
+        self._on_fn = on_fn
+        self._off_fn = off_fn
+        self._status = initial
+        self.set(initial)
+
+    def __bool__(self):
+        return self._status
+
+    def get(self):
+        """ Returns the state of the light. """
+        return self._status
+
+    def toggle(self):
+        """ Toggle the state of the light and returns updated state. """
+        self.set(not self._status)
+        return self._status
+
+    def set(self, state, force_update=False):
+        """ Sets the light on/off and sends the corresponding command.
+
+        An update command is only issued if the state changed. To force an
+        update command to be sent, set force_update to True.
+        :param state:  the state to set the light to (True for ON and False for
+        OFF).
+        :param force_update: set to True to force an update command to be issued
+        regardless of whether the light state changed.
+        """
+        dirty = self._status != state
+        self._status = state
+
+        if dirty or force_update:
+            if state:
+                if self._on_fn is not None:
+                    self._on_fn()
+            else:
+                if self._off_fn is not None:
+                    self._off_fn()
+
+
+class ColorLight(Light):
+    """ Controls a colored light source represented by a value.
+
+    Colors can be represented as an RGB color with 24 bits of the 32-bit integer
+    holding the red, green, blue channel information. Colors can also be
+    specially coded by an enumeration like value where different values map to
+    a preset color. This class does not distinguish between the two and simply
+    provides an integer value that the user can set. The provided update
+    function (which is device dependent) will provide this implementation.
+    """
+    def __init__(self, update_fn=None, initial=0):
+        self._color = initial
+        self._update_fn = update_fn
+
+    def get(self):
+        """ Returns the value of the light set. """
+        return self._color
+
+    def set(self, color, force_update=False):
+        """ Sets the light color and sends the corresponding command.
+
+        An update command is only issued if the state changed. To force an
+        update command to be sent, set force_update to True.
+        :param color:  the color value to set the light to
+        :param force_update: set to True to force an update command to be issued
+        regardless of whether the light color value changed.
+        """
+        dirty = self._color != color
+        self._color = color
+        if dirty or force_update:
+            if self._update_fn is not None:
+                self._update_fn(color)
+
+    def __bool__(self):
+        return self._color != 0
+
+
+class ColorToggleLight(ToggleLight):
+    """ Adapts a ColorLight so that it behaves as a toggle light. """
+
+    def __init__(self, light: Light, off_color=0, on_color=0x7F):
+        self._light = light
+        self._off_color = off_color
+        self._on_color = on_color
+
+    def __bool__(self):
+        return self._light.get() != self._off_color
+
+    def toggle(self):
+        if self._light.get() == self._off_color:
+            self._light.set(self._on_color)
+        else:
+            self._light.set(self._off_color)
+        return self._light.get() != self._off_color
+
+    def get(self):
+        return self._light.get()
+
+    def set(self, value, force_update=False):
+        self._light.set(value, force_update=force_update)
+
---
+++ tests/rum_lights_test.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from rum_lights import OnOffLight, ColorLight, ColorToggleLight
+
+
+class OnOffLightTests(unittest.TestCase):
+    def setUp(self):
+        self._on_count = 0
+        self._off_count = 0
+
+        def turn_on(): self._on_count += 1
+        def turn_off(): self._off_count += 1
+
+        self._light = OnOffLight(on_fn=turn_on, off_fn=turn_off)
+
+    def test_initialState_verifyStateAndNoCommandsSent(self):
+        self.assertFalse(self._light)
+        self.assertEqual(False, self._light.get())
+        self.assertEqual(0, self._on_count)
+        self.assertEqual(0, self._off_count)
+
+    def test_toggleState_verifyUpdatedStateAndCommandSent(self):
+        self._light.toggle()
+        self.assertTrue(self._light)
+        self.assertTrue(self._light.get())
+        self.assertEqual(1, self._on_count)
+        self.assertEqual(0, self._off_count)
+
+    def test_setOffFromInitialState_doesNotSendExtraUpdate(self):
+        self._light.set(False)
+        self.assertFalse(self._light)
+        self.assertFalse(self._light.get())
+        self.assertEqual(0, self._on_count)
+        self.assertEqual(0, self._off_count)
+
+    def test_setOnAfterToggle_doesNotSendExtraUpdate(self):
+        self._light.toggle()
+        self._light.set(True)
+        self.assertTrue(self._light)
+        self.assertTrue(self._light.get())
+        self.assertEqual(1, self._on_count)
+        self.assertEqual(0, self._off_count)
+
+    def test_setSameStateWithForcedUpdate_sendsUpdate(self):
+        self._light.toggle()
+        self._light.set(True, force_update=True)
+        self.assertTrue(self._light)
+        self.assertTrue(self._light.get())
+        self.assertEqual(2, self._on_count)
+        self.assertEqual(0, self._off_count)
+
+
+class ColorLightTests(unittest.TestCase):
+    def setUp(self):
+        self._update_values = []
+        def update_fn(value): self._update_values.append(value)
+        self._light = ColorLight(update_fn=update_fn)
+
+    def test_initialState_hasCorrectStateAndNoCommandsSent(self):
+        self.assertFalse(self._light)
+        self.assertEqual(0, self._light.get())
+        self.assertEqual(0, len(self._update_values))
+
+    def test_setDifferentValue_updatesNewValueSendsCommand(self):
+        self._light.set(15)
+        self.assertTrue(self._light)
+        self.assertEqual(15, self._light.get())
+        self.assertEqual([15], self._update_values)
+
+        self._light.set(16)
+        self.assertTrue(self._light)
+        self.assertEqual(16, self._light.get())
+        self.assertEqual([15, 16], self._update_values)
+
+    def test_setSameValue_noAdditionalCommandsSent(self):
+        self._light.set(15)
+        self._light.set(15)
+        self.assertTrue(self._light)
+        self.assertEqual(15, self._light.get())
+        self.assertEqual([15], self._update_values)
+
+
+class ColorToggleLightsTest(unittest.TestCase):
+    def setUp(self):
+        self._update_values = []
+        def update_fn(value): self._update_values.append(value)
+        light = ColorLight(update_fn=update_fn, initial=15)
+        self._light = ColorToggleLight(light, off_color=15, on_color=30)
+
+    def test_whenColorIsEquivalentToOffColor_toggleFlipsToOn(self):
+        self.assertEqual(True, self._light.toggle())
+        self.assertEqual(30, self._light.get())
+        self.assertTrue(self._light)
+        self.assertEqual([30], self._update_values)
+
+    def test_whenColorIsEquivalentToOnColor_toggleFlipsToOff(self):
+        self._light.toggle()
+        self.assertEqual(False, self._light.toggle())
+        self.assertEqual(15, self._light.get())
+        self.assertFalse(self._light)
+        self.assertEqual([30, 15], self._update_values)
+
+
+if __name__ == '__main__':
+    unittest.main()
