29f84be7704aca4741d7c8ed64f28be9a25eef7c
Ray Juang <rjuang@github.com>
2021-01-06 20:17:59 -0800

    Finish adding tests for processor.

--- tests/processor_test.py
+++ tests/processor_test.py
@@ -1,63 +1,159 @@
+import sys
 import unittest
 
+sys.path.insert(0, '..')
 
-# TODO: Fill in unit tests below
+from rum import processor
+from rum.matchers import status_eq, data1_eq
+from rum.midi import MidiMessage
+from rum.processor import MidiProcessor, When, WhenAll, WhenAny, trigger_when
+
 
 class MidiProcessorTest(unittest.TestCase):
+    def setUp(self):
+        self._processor = MidiProcessor()
+
     def test_nothingToProcess_processCalledDoesNothing(self):
-        pass
+        self._processor.process(MidiMessage(0x128, 0x28, 0x30))
+        # Passes as long as this doesn't crash
 
-    def test_failingTriggerToProcess_processCalledDoesNothing(self):
-        pass
+    def test_processFunctionAdded_nothingCalledIfProcessNotCalled(self):
+        self._cnt = 0
 
-    def test_passingTriggerToProcess_processCalledOnPassingTrigger(self):
-        pass
+        def process(_):
+            self._cnt += 1
 
-    def test_failedAndPassedTriggersToProcess_onlyPassedTriggers(self):
-        pass
+        self._processor.add(process)
+        self.assertEqual(0, self._cnt)
+
+    def test_processFunctionAdded_fnCalledWhenProcessCalled(self):
+        self._cnt = 0
+
+        def process(_):
+            self._cnt += 1
+
+        self._processor.add(process)
+        self._processor.process(MidiMessage(128, 3, 4))
+        self.assertEqual(1, self._cnt)
+
+    def test_processFunctionAdded_multipleProcessCall(self):
+        self._cnt = 0
+
+        def process(_):
+            self._cnt += 1
+
+        self._processor.add(process)
+        self._processor.process(MidiMessage(128, 3, 4))
+        self._processor.process(MidiMessage(128, 3, 5))
+        self._processor.process(MidiMessage(128, 3, 4))
+        self.assertEqual(3, self._cnt)
 
 
 class WhenTest(unittest.TestCase):
     def test_whenConditionFails_doesNotTriggerThen(self):
-        pass
+        self._cnt = 0
+
+        def increment_cnt(_):
+            self._cnt += 1
+
+        process = When(status_eq(34)).then(increment_cnt)
+        process(MidiMessage(35, 1, 1))
+        self.assertEqual(0, self._cnt)
 
     def test_whenConditionPasses_triggersThen(self):
-        pass
+        self._received = []
+
+        def dispatch(m):
+            self._received.append(m)
+
+        process = When(status_eq(34)).then(dispatch)
+        m = MidiMessage(34, 1, 1)
+        process(m)
+        self.assertEqual([m], self._received)
 
 
 class WhenAllTest(unittest.TestCase):
     def test_whenSingleConditionFails_doesNotTriggerThen(self):
-        pass
+        self._received = []
+
+        def dispatch(m):
+            self._received.append(m)
+
+        process = WhenAll(status_eq(33), data1_eq(0)).then(dispatch)
+        m = MidiMessage(33, 1, 1)
+        process(m)
+        self.assertEqual([], self._received)
 
     def test_whenAllConditionsPass_triggersThen(self):
-        pass
+        self._received = []
+
+        def dispatch(m):
+            self._received.append(m)
+
+        process = WhenAll(status_eq(34), data1_eq(1)).then(dispatch)
+        m = MidiMessage(34, 1, 1)
+        process(m)
+        self.assertEqual([m], self._received)
 
     def test_whenNoConditions_triggersThen(self):
-        pass
+        self._received = []
 
+        def dispatch(m):
+            self._received.append(m)
 
-class WhenAllTest(unittest.TestCase):
-    def test_whenSingleConditionFails_doesNotTriggerThen(self):
-        pass
-
-    def test_whenAllConditionsPass_triggersThen(self):
-        pass
+        process = WhenAll(status_eq(34), data1_eq(2)).then(dispatch)
+        m = MidiMessage(33, 1, 1)
+        process(m)
+        self.assertEqual([], self._received)
 
 
 class WhenAnyTest(unittest.TestCase):
     def test_whenAllConditionFails_doesNotTriggerThen(self):
-        pass
+        self._received = []
+
+        def dispatch(m):
+            self._received.append(m)
+
+        process = WhenAny(status_eq(34), data1_eq(1)).then(dispatch)
+        m = MidiMessage(33, 0, 1)
+        process(m)
+        self.assertEqual([], self._received)
 
     def test_whenSingleConditionsPass_triggersThen(self):
-        pass
+        self._received = []
+
+        def dispatch(m):
+            self._received.append(m)
+
+        process = WhenAny(status_eq(34), data1_eq(1)).then(dispatch)
+        m = MidiMessage(33, 1, 1)
+        process(m)
+        self.assertEqual([m], self._received)
 
 
 class TriggerWhenTest(unittest.TestCase):
     def test_triggerWhenPassAndFailCondition_doesNotCallOnProcess(self):
-        pass
+        self._received = []
+
+        @trigger_when(status_eq(128), data1_eq(1))
+        def dispatch(m):
+            self._received.append(m)
+
+        m = MidiMessage(128, 0, 2)
+        processor.get_processor().process(m)
+        self.assertEqual([], self._received)
 
     def test_triggerWhenAllPass_callsFunctionOnProcess(self):
-        pass
+        self._received = []
+
+        @trigger_when(status_eq(128), data1_eq(1))
+        def dispatch(m):
+            self._received.append(m)
+
+        m = MidiMessage(128, 1, 2)
+        processor.get_processor().process(m)
+        self.assertEqual([m], self._received)
+
 
 if __name__ == '__main__':
     unittest.main()
